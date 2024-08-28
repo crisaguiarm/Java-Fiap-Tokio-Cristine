d1050b16bc9a683e164e9500fb4ef6639a1ef935
“Cristine <cristineaguiarm@gmail.com>
2024-08-28 18:00:33 -0300

    Primeiro commit

---
+++ Fiap_Cristine_1/src/Exercicios/aula6/Cristine/Exercicio1.java
@@ -0,0 +1,18 @@
+package Exercicios.aula6.Cristine;
+import java.util.Scanner;
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		// Faça um programa que leia um número, e informe se ele é par ou impar.
+		
+		Scanner entrada1 = new Scanner(System.in);
+		System.out.print("Digite um numero: ");
+		int num = entrada1.nextInt();
+		if (num % 2 = 0) {
+			System.out.println("Par");
+	} else {
+	System.out.println("Impar");
+	}
+	}
+
+}
